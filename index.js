402b698dfa6ada9b2ba32856c921c06ae6a3cf5b
camilanicola <camilanicolads@gmail.com>
2024-07-10 01:53:23 -0300

    Commit 3 - Mais Extract Functions

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const { readFileSync } = require('fs');
 
-function gerarFaturaStr (fatura, pecas) {
+function gerarFaturaStr (fatura) {
     let totalFatura = 0;
     let creditos = 0;
     let faturaStr = `Fatura ${fatura.cliente}\n`;
-    const formato = new Intl.NumberFormat("pt-BR",
-                          { style: "currency", currency: "BRL",
-                            minimumFractionDigits: 2 }).format;
-  
+    
     for (let apre of fatura.apresentacoes) {
       let total=calcularTotalApresentacao(apre, getPeca(apre));
   
       // créditos para próximas contratações
-      creditos += Math.max(apre.audiencia - 30, 0);
-      if (getPeca(apre).tipo === "comedia") 
-         creditos += Math.floor(apre.audiencia / 5);
+      creditos += calcularCredito(apre);
   
       // mais uma linha da fatura
-      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(total/100)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
-    faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
+    faturaStr += `Valor total: ${formatarMoeda(totalFatura/100)}\n`;
     faturaStr += `Créditos acumulados: ${creditos} \n`;
     return faturaStr;
   }
@@ -48,6 +43,18 @@ function calcularTotalApresentacao(apre){
 }
 function getPeca(apresentacao) {
   return pecas[apresentacao.id];
+}
+function calcularCredito(apre) {
+  let creditos = 0;
+  creditos += Math.max(apre.audiencia - 30, 0);
+  if (getPeca(apre).tipo === "comedia") 
+     creditos += Math.floor(apre.audiencia / 5);
+  return creditos;   
+}
+function formatarMoeda(valor) {
+  return new Intl.NumberFormat("pt-BR",
+    { style: "currency", currency: "BRL",
+      minimumFractionDigits: 2 }).format(valor);
 }
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
